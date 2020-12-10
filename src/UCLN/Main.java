226aabf1666b9da7bd2ea0e6a42ccbf9ee9d8fff
trungbe <trungnd0601@gmail.com>
2020-12-10 09:24:31 +0700

    tim ucln

---
+++ src/UCLN/Main.java
@@ -0,0 +1,29 @@
+package UCLN;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = sc.nextInt();
+        System.out.println("Enter b: ");
+        b = sc.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0)
+            System.out.println("No greatest common factor");
+
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
+}
